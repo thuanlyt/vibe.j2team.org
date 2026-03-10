cbcb9248ffb7b1fb08f46874c4139ec15bc92e94
Thuận LYT <lytz.art@gmail.com>
2026-03-10 15:24:31 +0700

    Update meta.ts

--- src/views/bomberman/meta.ts
+++ src/views/bomberman/meta.ts
@@ -1,6 +1,12 @@
-export default {
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
   name: 'Mini Bomberman',
-  description: 'Trò chơi Đặt Bom cổ điển phiên bản Emoji. Tiêu diệt Bot để chiến thắng!',
-  author: 'Vibe Coder',
-  icon: '💣',
+  description:
+    'Trò chơi Đặt Bom cổ điển phiên bản Emoji. Tiêu diệt Bot để chiến thắng!',
+  author: 'J2TEAM',
+  facebook: 'https://www.facebook.com/groups/j2team.community',
+  category: 'learn',
 }
+
+export default meta
